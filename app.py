9f964817611f1fd842110c8819f9de2f8460c996
Rilepro <veljkorile55@gmail.com>
2022-06-19 15:01:55 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask
+from flask_restful import  Api
+from flask_jwt import JWT
+
+
+from security import authenticate, identity
+from resource.user import UserRegister
+from resource.item import Item, ItemList
+
+app= Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) #/auth
+
+
+
+api.add_resource(Item, '/item/<string:name>')  #http://127.0.0.1:5000/item/chair
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+
+    app.run(port=5000, debug=True)
+
+
+
+
+
